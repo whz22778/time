3493d00940b4f730ceb16d36a6d63e157143d76f
whz <whz22778@qq.com>
2026-03-15 21:26:16 +0800

    [feat]增加KDD数据集载入

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader,NB15
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader,NB15,KDD
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
@@ -17,6 +17,7 @@ data_dict = {
     'SWAT': SWATSegLoader,
     'UEA': UEAloader,
     'NB15': NB15
+    'KDD': KDD
 }
 
 
