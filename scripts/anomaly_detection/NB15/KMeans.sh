7416c328947ad43bdc53b5cda6d54aa26bbc85d9
a <>
2026-04-02 21:41:37 +0800

    [feat]确定NB_k参数

--- scripts/anomaly_detection/NB15/KMeans.sh
+++ scripts/anomaly_detection/NB15/KMeans.sh
@@ -4,13 +4,13 @@ python -u run.py \
   --task_name anomaly_detection \
   --is_training 1 \
   --root_path ./dataset/NB15 \
-  --model_id NB15 \
+  --model_id NB15_7c \
   --model kmeans \
   --data NB15 \
   --features M \
   --seq_len 1 \
   --pred_len 0 \
-  --anomaly_ratio 45 \
+  --anomaly_ratio 80 \
   --batch_size 128 \
   --train_epochs 1 \
-  --kmeans_n_clusters 6
+  --kmeans_n_clusters 7
