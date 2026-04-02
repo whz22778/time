d9574f4386e16d67e140af4fb2bb4b82ccc103b0
a <>
2026-04-02 21:12:18 +0800

    [feat]确定kdd_can参数

--- scripts/anomaly_detection/KDD/DBSCAN.sh
+++ scripts/anomaly_detection/KDD/DBSCAN.sh
@@ -4,7 +4,7 @@ python -u run.py \
   --task_name anomaly_detection \
   --is_training 1 \
   --root_path ./dataset/KDD \
-  --model_id KDD \
+  --model_id KDD_005_15s \
   --model dbscan \
   --data KDD \
   --features M \
@@ -13,5 +13,5 @@ python -u run.py \
   --anomaly_ratio 35 \
   --batch_size 64 \
   --train_epochs 1 \
-  --dbscan_eps 0.6 \
-  --dbscan_min_samples 5
+  --dbscan_eps 0.05 \
+  --dbscan_min_samples 15
