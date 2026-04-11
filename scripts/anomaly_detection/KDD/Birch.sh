c2f8d6b771af947e3f1af666170581ff971d8f1c
a <>
2026-04-11 16:06:45 +0800

    [set]KDD_b参数

--- scripts/anomaly_detection/KDD/Birch.sh
+++ scripts/anomaly_detection/KDD/Birch.sh
@@ -4,14 +4,14 @@ python -u run.py \
   --task_name anomaly_detection \
   --is_training 1 \
   --root_path ./dataset/KDD \
-  --model_id KDD \
+  --model_id KDD_60f_35t_30 \
   --model birch \
   --data KDD \
   --features M \
   --seq_len 1 \
   --pred_len 0 \
-  --anomaly_ratio 35 \
+  --anomaly_ratio 30 \
   --batch_size 64 \
   --train_epochs 1 \
-  --birch_threshold 0.25 \
-  --birch_branching_factor 50
+  --birch_threshold 0.35 \
+  --birch_branching_factor 60
