4677a3654a9d805b74fe159daf47c860c315c997
a <>
2026-04-12 19:33:04 +0800

    [set]NB_ts_be

--- scripts/anomaly_detection/NB15/Transformer.sh
+++ scripts/anomaly_detection/NB15/Transformer.sh
@@ -2,19 +2,19 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -u run.py \
   --task_name anomaly_detection \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/NB15 \
   --model_id NB15 \
   --model Transformer \
   --data NB15 \
   --features M \
-  --seq_len 100 \
+  --seq_len 10 \
   --pred_len 0 \
   --d_model 64 \
   --d_ff 128 \
   --e_layers 3 \
   --enc_in 23 \
   --c_out 23 \
-  --anomaly_ratio 1 \
+  --anomaly_ratio 45 \
   --batch_size 128 \
   --train_epochs 3
