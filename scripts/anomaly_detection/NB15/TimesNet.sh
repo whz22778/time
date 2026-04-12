c600473bc0272ece3fddee81f2861dd666a66898
a <>
2026-04-12 16:32:38 +0800

    [set]NB_ti参数

--- scripts/anomaly_detection/NB15/TimesNet.sh
+++ scripts/anomaly_detection/NB15/TimesNet.sh
@@ -2,13 +2,13 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -u run.py \
   --task_name anomaly_detection \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/NB15 \
   --model_id NB15 \
   --model TimesNet \
   --data NB15 \
   --features M \
-  --seq_len 100 \
+  --seq_len 20 \
   --pred_len 0 \
   --d_model 64 \
   --d_ff 128 \
