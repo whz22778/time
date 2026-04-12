fc9aa93ab1a20a1b439473a670546f11bb562820
a <>
2026-04-12 17:44:26 +0800

    [set]NB_brich

--- scripts/anomaly_detection/NB15/Birch.sh
+++ scripts/anomaly_detection/NB15/Birch.sh
@@ -4,14 +4,14 @@ python -u run.py \
   --task_name anomaly_detection \
   --is_training 1 \
   --root_path ./dataset/NB15 \
-  --model_id NB15 \
+  --model_id NB15_95f_45t \
   --model birch \
   --data NB15 \
   --features M \
   --seq_len 1 \
   --pred_len 0 \
-  --anomaly_ratio 40 \
+  --anomaly_ratio 0.5 \
   --batch_size 128 \
   --train_epochs 1 \
-  --birch_threshold 0.1 \
-  --birch_branching_factor 100
+  --birch_threshold 0.45 \
+  --birch_branching_factor 95
