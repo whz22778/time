496fb72d69c3b26d5a19ed8f44e1ac53c1833d5b
a <>
2026-04-14 18:19:45 +0800

    [set]NB_ti

--- scripts/anomaly_detection/NB15/TimesNet.sh
+++ scripts/anomaly_detection/NB15/TimesNet.sh
@@ -8,14 +8,14 @@ python -u run.py \
   --model TimesNet \
   --data NB15 \
   --features M \
-  --seq_len 20 \
+  --seq_len 15 \
   --pred_len 0 \
   --d_model 64 \
-  --d_ff 128 \
-  --e_layers 2 \
+  --d_ff 64 \
+  --e_layers 3 \
   --enc_in 23 \
   --c_out 23 \
-  --top_k 3 \
-  --anomaly_ratio 50 \
+  --top_k 1 \
+  --anomaly_ratio 55 \
   --batch_size 128 \
-  --train_epochs 3
+  --train_epochs 5
