1917939137e8be2ef4fa242f1bdc164d74cad0c7
a <>
2026-04-20 16:00:15 +0800

    [set]CIC_Ti

--- scripts/anomaly_detection/CIC/TimesNet.sh
+++ scripts/anomaly_detection/CIC/TimesNet.sh
@@ -2,20 +2,20 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -u run.py \
   --task_name anomaly_detection \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/CIC \
-  --model_id CIC \
+  --model_id CIC_1K_256b \
   --model TimesNet \
   --data CIC \
   --features M \
-  --seq_len 100 \
+  --seq_len 40 \
   --pred_len 0 \
   --d_model 64 \
   --d_ff 128 \
   --e_layers 2 \
   --enc_in 20 \
   --c_out 20 \
-  --top_k 3 \
-  --anomaly_ratio 1 \
-  --batch_size 128 \
+  --top_k 1 \
+  --anomaly_ratio 6 \
+  --batch_size 256 \
   --train_epochs 3
