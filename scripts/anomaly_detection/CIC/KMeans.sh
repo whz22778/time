f3e4d9b32a2727ec0b25fd48250b0c56f6b9530e
a <>
2026-04-22 23:41:22 +0800

    [set]CIC_kmeans

--- scripts/anomaly_detection/CIC/KMeans.sh
+++ scripts/anomaly_detection/CIC/KMeans.sh
@@ -4,13 +4,13 @@ python -u run.py \
   --task_name anomaly_detection \
   --is_training 1 \
   --root_path ./dataset/CIC \
-  --model_id CIC \
+  --model_id CIC_6k_18 \
   --model kmeans \
   --data CIC \
   --features M \
   --seq_len 1 \
   --pred_len 0 \
-  --anomaly_ratio 4 \
-  --batch_size 128 \
+  --anomaly_ratio 18 \
+  --batch_size 256 \
   --train_epochs 1 \
-  --kmeans_n_clusters 5
+  --kmeans_n_clusters 6
