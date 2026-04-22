af5d309c44ecc631473934efb4e3bfe5ff07e068
a <>
2026-04-22 23:41:22 +0800

    [set]CIC_ts

--- scripts/anomaly_detection/CIC/Transformer.sh
+++ scripts/anomaly_detection/CIC/Transformer.sh
@@ -2,19 +2,19 @@ export CUDA_VISIBLE_DEVICES=0
 
 python -u run.py \
   --task_name anomaly_detection \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/CIC \
   --model_id CIC \
   --model Transformer \
   --data CIC \
   --features M \
-  --seq_len 100 \
+  --seq_len 30 \
   --pred_len 0 \
   --d_model 128 \
   --d_ff 128 \
   --e_layers 3 \
   --enc_in 20 \
   --c_out 20 \
-  --anomaly_ratio 1 \
-  --batch_size 128 \
+  --anomaly_ratio 15 \
+  --batch_size 256 \
   --train_epochs 3
