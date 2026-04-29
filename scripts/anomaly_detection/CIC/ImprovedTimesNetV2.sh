6a3684190261a5153c75d9cfa25463f76297a23f
a <>
2026-04-29 19:49:52 +0800

    [set]CIC_ti2

--- scripts/anomaly_detection/CIC/ImprovedTimesNetV2.sh
+++ scripts/anomaly_detection/CIC/ImprovedTimesNetV2.sh
@@ -1,14 +1,14 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python -u run.py \
+  python -u run.py \
   --task_name anomaly_detection \
-  --is_training 1 \
+  --is_training 0 \
   --root_path ./dataset/CIC \
-  --model_id CIC_ImprovedTimesNetV2 \
+  --model_id CIC_5k \
   --model ImprovedTimesNetV2 \
   --data CIC \
   --features M \
-  --seq_len 40 \
+  --seq_len 30 \
   --pred_len 0 \
   --d_model 64 \
   --d_ff 128 \
@@ -16,11 +16,12 @@ python -u run.py \
   --enc_in 20 \
   --c_out 20 \
   --top_k 1 \
-  --n_clusters 5 \
-  --feature_k 6 \
+  --n_clusters 6 \
+  --feature_k 5 \
   --interaction_topk fixed \
   --cluster_init kmeans \
-  --cluster_freeze \
-  --anomaly_ratio 1 \
-  --batch_size 128 \
-  --train_epochs 3
+  --cluster_freeze 1 \
+  --anomaly_ratio 6 \
+  --batch_size 256 \
+  --train_epochs 3 \
+  --result 1
